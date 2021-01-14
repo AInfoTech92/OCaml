300d750ea086ef0f752d787ebaccfb3018f35edd
Alessio Demurtas <alessio.demurtas92@gmail.com>
2021-01-14 18:21:27 +0100

    Third Exercise

---
+++ ex_003.ml
@@ -0,0 +1,13 @@
+(*
+3. Find the k'th element of a list. (easy)
+*)
+
+let rec at i l =
+	match l with
+	| [] -> None
+	| h::t when i = 1 -> Some h
+	| h::t -> at (i-1) t;;
+
+at 1 [1;2;3];;
+at 3 [ "a" ; "b"; "c"; "d"; "e" ];;
+at 3 [ "a" ];;
