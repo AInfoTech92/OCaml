006a047cfda3d0e49ed156e20ad4a395c3642d93
Alessio Demurtas <alessio.demurtas92@gmail.com>
2021-01-14 18:43:52 +0100

    4-th Exercise

---
+++ ex_004.ml
@@ -0,0 +1,15 @@
+(*
+4. Find the number of elements of a list. (easy)
+*)
+
+let length l =
+	let rec aux i ls =
+		match ls with
+		| [] -> i
+		| h::t -> aux (i+1) t
+	in
+		aux 0 l;;
+
+length [1;2;3];;
+length [ "a" ; "b"; "c"; "d"; "e" ];;
+length [ "a" ];;
